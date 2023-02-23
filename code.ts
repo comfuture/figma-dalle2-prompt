e1d42f01572066f10ec3b41318cdb31812b263fd
comfuture <comfuture@iplateia.com>
2023-02-23 17:02:17 +0900

    refactory backend code

--- code.ts
+++ code.ts
@@ -1,43 +1,86 @@
 figma.showUI(__html__, { themeColors: true });
 figma.ui.resize(512, 512);
 
-figma.ui.onmessage = async (msg) => {
-  // load the api key from client storage
-  if (msg.type === 'preferences.read') {
-    const apiKey = await figma.clientStorage.getAsync('api_key');
-    figma.ui.postMessage({ type: 'preferences.loaded', data: apiKey });
+type IPCMessageType = 'preferences.read' | 'preferences.save' | 'image.data';
 
+type IPCMessage = {
+  type: IPCMessageType;
+  [key: string]: any;
+}
+
+/** load the api key from client storage */
+async function loadPreferences() {
+  const apiKey = await figma.clientStorage.getAsync('api_key');
+  figma.ui.postMessage({ type: 'preferences.loaded', data: apiKey });
+}
+
+/** save the api key to client storage */
+async function savePreferences(apiKey: string) {
+  await figma.clientStorage.setAsync('api_key', apiKey);
+}
+
+/** creates an image frame and adds it to the page */
+async function createImage(prompt: string, imageData: Uint8Array, width: number = 1024, height: number = 1024) {
+  const nodes: SceneNode[] = [];
+
+  const frame = figma.createFrame();
+  frame.resizeWithoutConstraints(1024, 1024);
+  frame.name = prompt;
+
+  const image = figma.createImage(imageData);
+  const paint: ImagePaint = {
+    type: 'IMAGE',
+    imageHash: image.hash,
+    scaleMode: "FIT",
   }
+  const imageNode = figma.createRectangle();
+  imageNode.resizeWithoutConstraints(width, height);
+  imageNode.fills = [paint];
+  imageNode.name = prompt;
 
-  // save the api key to client storage
-  if (msg.type === 'preferences.save') {
-    await figma.clientStorage.setAsync('api_key', msg.data);
-  }
+  frame.appendChild(imageNode);
 
-  // create a frame and image node from the image data
-  if (msg.type === 'image.data') {
-    const nodes: SceneNode[] = [];
-
-    const frame = figma.createFrame();
-    frame.resizeWithoutConstraints(1024, 1024);
-    frame.name = msg.prompt;
-
-    const image = figma.createImage(msg.im);
-    const paint: ImagePaint = {
-      type: 'IMAGE',
-      imageHash: image.hash,
-      scaleMode: "FIT",
+  // position the frame
+  let minX = 0
+  let maxY = 0
+  figma.skipInvisibleInstanceChildren = true
+  function traverse(node: BaseNode) {
+    if ("children" in node) {
+      if (node.type === "FRAME") {
+        minX = Math.min(minX, node.x)
+        maxY = Math.max(maxY, node.y + node.height)
+      }
+      if (node.type !== "INSTANCE") {
+        for (const child of node.children) {
+          traverse(child)
+        }
+      }
     }
-    const imageNode = figma.createRectangle();
-    imageNode.resizeWithoutConstraints(1024, 1024);
-    imageNode.fills = [paint];
-    imageNode.name = msg.prompt;
-
-    frame.appendChild(imageNode);
-    figma.currentPage.appendChild(frame);
-    nodes.push(frame);
-    figma.currentPage.selection = nodes;
-    // figma.viewport.scrollAndZoomIntoView(nodes);
-    figma.closePlugin();
   }
-};
+  traverse(figma.root)
+
+  frame.x = minX
+  frame.y = maxY + 40
+
+  figma.closePlugin()
+
+  figma.currentPage.appendChild(frame);
+  nodes.push(frame);
+  figma.currentPage.selection = nodes;
+  // figma.viewport.scrollAndZoomIntoView(nodes);
+}
+
+figma.ui.onmessage = async (msg: IPCMessage) => {
+  switch (msg.type) {
+    case 'preferences.read':
+      await loadPreferences();
+      break;
+    case 'preferences.save':
+      await savePreferences(msg.data);
+      break;
+    case 'image.data':
+      await createImage(msg.prompt, msg.im);
+      figma.closePlugin();
+      break;
+  }
+}
